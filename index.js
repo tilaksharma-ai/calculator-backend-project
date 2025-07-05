95c2e49df62066e6e98c421e1b06e105a5e3f084
Tilak Sharma <tilak.sharma.ai@gmail.com>
2025-07-05 03:18:41 +0000

    Create a function to welcome the user

--- index.js
+++ index.js
@@ -1,27 +1,33 @@
-function sum(a,b){
-    let sumAll = 0;
-    for (a; a<=b;a++){ // Didn't create "i" variable because no need for that
-        sumAll += a;
-    }
-    return sumAll;
+function sum(a, b) {
+  let sumAll = 0;
+  for (a; a <= b; a++) {
+    // Didn't create "i" variable because no need for that
+    sumAll += a;
+  }
+  return sumAll;
 }
 
-function add(a,b){
-    return a+b;
+function add(a, b) {
+  return a + b;
 }
 
-function subtract(a,b){
-    return a-b;
+function subtract(a, b) {
+  return a - b;
 }
 
-function multiply(a,b){
-    return a*b;
+function multiply(a, b) {
+  return a * b;
 }
 
-function divide(a,b){
-    if (b===0){
-        return "Can't divide with 0!"
-    }
-    return a/b;
+function divide(a, b) {
+  if (b === 0) {
+    return "Can't divide with 0!";
+  }
+  return a / b;
+}
+
+function welcome(){
+    message = '=== JavaScript Calculator ===';
+    console.log(message);
 }
 
